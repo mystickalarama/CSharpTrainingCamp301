6546bb45bbe51974ea87cc6255dad76ab139d2b9
Sinan Kerem Doğan <kerem_force@hotmail.com>
2024-11-19 17:04:01 +0300

    Data Access and Context Class

--- CSharpTrainingCamp301.EntityLayer/Concrete/Customer.cs
+++ CSharpTrainingCamp301.EntityLayer/Concrete/Customer.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace CSharpTrainingCamp301.EntityLayer.Concrete
 {
-	internal class Customer
+	public class Customer
 	{
 		public int CustomerId { get; set; }
 		public string CustomerName { get; set; }
 		public string CustomerSurname { get; set; }
 		public string CostomerDistrict { get; set; }
 		public string CostomerCity { get; set; }
-	}
+        public List<Order> Orders { get; set; }
+    }
 }
 
 /* 
